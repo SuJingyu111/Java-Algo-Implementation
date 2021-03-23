c296aafe90219447189a1090db8c1899b08342c7
SuJingyu111 <gar123su@gmail.com>
2021-03-23 15:00:26 +0800

    Counting Sort

---
+++ src/SortAlgos/CountingSort.java
@@ -0,0 +1,32 @@
+package SortAlgos;
+
+import java.util.Arrays;
+
+public class CountingSort {
+    public static Integer[] countingSort(Integer[] array) {
+        Integer[] resultArray = new Integer[array.length];
+        Integer maxVal = array[0];
+        for (Integer i : array) {
+            maxVal = Math.max(maxVal, i);
+        }
+        Integer[] countingArray = new Integer[maxVal];
+        Arrays.fill(countingArray, 0);
+        for (Integer i : array) {
+            countingArray[i] += 1;
+        }
+        for (int i = 1; i < countingArray.length; i++) {
+            countingArray[i] += countingArray[i - 1];
+        }
+        for (int i = array.length - 1; i >= 0; i-- ) {
+            resultArray[countingArray[array[i]]] = array[i];
+            countingArray[array[i]] -= 1;
+        }
+        return resultArray;
+    }
+    public static void main(String[] args) {
+        Integer[] array = {5, 234, 34, 67, 123, 76, 8, 38};
+        QuickSort.quickSort(array);
+        System.out.println(Arrays.toString(array));
+        SortTest.test(QuickSort.class);
+    }
+}
